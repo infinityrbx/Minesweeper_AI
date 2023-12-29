4c59523a87cfb1c09773d9a1aa84f1704f6b9563
infinityrbx <131098555+infinityrbx@users.noreply.github.com>
2023-12-29 15:44:33 +0700

    Add files via upload

---
+++ Finished code/runner.py
@@ -0,0 +1,227 @@
+import pygame
+import sys
+import time
+
+from minesweeper import Minesweeper, MinesweeperAI
+
+HEIGHT = 8
+WIDTH = 8
+#mine_config
+MINES = 13
+
+# Colors
+BLACK = (0, 0, 0)
+GRAY = (180, 180, 180)
+WHITE = (255, 255, 255)
+
+# Create game
+pygame.init()
+size = width, height = 600, 400
+screen = pygame.display.set_mode(size)
+
+# Fonts
+OPEN_SANS = "assets/fonts/OpenSans-Regular.ttf"
+smallFont = pygame.font.Font(OPEN_SANS, 20)
+mediumFont = pygame.font.Font(OPEN_SANS, 28)
+largeFont = pygame.font.Font(OPEN_SANS, 40)
+
+# Compute board size
+BOARD_PADDING = 20
+board_width = ((2 / 3) * width) - (BOARD_PADDING * 2)
+board_height = height - (BOARD_PADDING * 2)
+cell_size = int(min(board_width / WIDTH, board_height / HEIGHT))
+board_origin = (BOARD_PADDING, BOARD_PADDING)
+
+# Add images
+flag = pygame.image.load("assets/images/flag.png")
+flag = pygame.transform.scale(flag, (cell_size, cell_size))
+mine = pygame.image.load("assets/images/mine.png")
+mine = pygame.transform.scale(mine, (cell_size, cell_size))
+background_image = pygame.image.load("assets/images/background.jpg")
+background_image = pygame.transform.scale(background_image, (width, height))
+# Create game and AI agent
+game = Minesweeper(height=HEIGHT, width=WIDTH, mines=MINES)
+ai = MinesweeperAI(height=HEIGHT, width=WIDTH)
+
+# Keep track of revealed cells, flagged cells, and if a mine was hit
+revealed = set()
+flags = set()
+lost = False
+
+# Show instructions initially
+instructions = True
+
+while True:
+
+    
+
+    # Check if game quit
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+    screen.blit(background_image, (0, 0))
+
+    # Show game instructions
+    if instructions:
+
+        # Title
+        title = largeFont.render("Trò chơi dò mìn", True, BLACK)
+        titleRect = title.get_rect()
+        titleRect.center = ((width / 2), 50)
+        screen.blit(title, titleRect)
+
+        # Rules
+        rules = [
+            "By nhóm 36 (mã lớp 144915)",
+            "Nhấp vào một ô để mở nó.",
+            "Nhấp phải vào một ô để đánh dấu là ô mìn.",
+            "Đánh dấu đúng tất cả các ô mìn để chiến thắng!"
+        ]
+        for i, rule in enumerate(rules):
+            line = smallFont.render(rule, True, BLACK)
+            lineRect = line.get_rect()
+            lineRect.center = ((width / 2), 150 + 30 * i)
+            screen.blit(line, lineRect)
+
+        # Play game button
+        buttonRect = pygame.Rect((width / 4), (3 / 4) * height, width / 2, 50)
+        buttonText = mediumFont.render("Bắt đầu trò chơi", True, BLACK)
+        buttonTextRect = buttonText.get_rect()
+        buttonTextRect.center = buttonRect.center
+        pygame.draw.rect(screen, WHITE, buttonRect)
+        screen.blit(buttonText, buttonTextRect)
+
+        # Check if play button clicked
+        click, _, _ = pygame.mouse.get_pressed()
+        if click == 1:
+            mouse = pygame.mouse.get_pos()
+            if buttonRect.collidepoint(mouse):
+                instructions = False
+                time.sleep(0.3)
+
+        pygame.display.flip()
+        continue
+
+    # Draw board
+    cells = []
+    for i in range(HEIGHT):
+        row = []
+        for j in range(WIDTH):
+
+            # Draw rectangle for cell
+            rect = pygame.Rect(
+                board_origin[0] + j * cell_size,
+                board_origin[1] + i * cell_size,
+                cell_size, cell_size
+            )
+            pygame.draw.rect(screen, WHITE, rect)
+            pygame.draw.rect(screen, GRAY, rect, 3)
+
+            # Add a mine, flag, or number if needed
+            if game.is_mine((i, j)) and lost:
+                screen.blit(mine, rect)
+            elif (i, j) in flags:
+                screen.blit(flag, rect)
+            elif (i, j) in revealed:
+                neighbors = smallFont.render(
+                    str(game.nearby_mines((i, j))),
+                    True, BLACK
+                )
+                neighborsTextRect = neighbors.get_rect()
+                neighborsTextRect.center = rect.center
+                screen.blit(neighbors, neighborsTextRect)
+
+            row.append(rect)
+        cells.append(row)
+
+    # AI Move button
+    aiButton = pygame.Rect(
+        (2 / 3) * width + BOARD_PADDING, (1 / 3) * height - 50,
+        (width / 3) - BOARD_PADDING * 2, 50
+    )
+    buttonText = mediumFont.render("AI Chọn", True, BLACK)
+    buttonRect = buttonText.get_rect()
+    buttonRect.center = aiButton.center
+    pygame.draw.rect(screen, WHITE, aiButton)
+    screen.blit(buttonText, buttonRect)
+
+    # Reset button
+    resetButton = pygame.Rect(
+        (2 / 3) * width + BOARD_PADDING, (1 / 3) * height + 20,
+        (width / 3) - BOARD_PADDING * 2, 50
+    )
+    buttonText = mediumFont.render("Đặt lại", True, BLACK)
+    buttonRect = buttonText.get_rect()
+    buttonRect.center = resetButton.center
+    pygame.draw.rect(screen, WHITE, resetButton)
+    screen.blit(buttonText, buttonRect)
+
+    # Display text
+    text = "Thua!!" if lost else "Thắng!!" if game.mines == flags else ""
+    text = mediumFont.render(text, True, BLACK)
+    textRect = text.get_rect()
+    textRect.center = ((5 / 6) * width, (2 / 3) * height)
+    screen.blit(text, textRect)
+
+    move = None
+
+    left, _, right = pygame.mouse.get_pressed()
+
+    # Check for a right-click to toggle flagging
+    if right == 1 and not lost:
+        mouse = pygame.mouse.get_pos()
+        for i in range(HEIGHT):
+            for j in range(WIDTH):
+                if cells[i][j].collidepoint(mouse) and (i, j) not in revealed:
+                    if (i, j) in flags:
+                        flags.remove((i, j))
+                    else:
+                        flags.add((i, j))
+                    time.sleep(0.2)
+
+    elif left == 1:
+        mouse = pygame.mouse.get_pos()
+
+        # If AI button clicked, make an AI move
+        if aiButton.collidepoint(mouse) and not lost:
+            move = ai.make_safe_move()
+            if move is None:
+                move = ai.make_random_move()
+                if move is None:
+                    flags = ai.mines.copy()
+                    print("Không còn nước đi nào, Trò chơi kết thúc")
+                else:
+                    print("Không ô nào an toàn, AI tìm nước đi ngẫu nhiên")
+            else:
+                print("AI đang tìm nước đi tiếp theo.")
+            time.sleep(0.2)
+
+        # Reset game state
+        elif resetButton.collidepoint(mouse):
+            game = Minesweeper(height=HEIGHT, width=WIDTH, mines=MINES)
+            ai = MinesweeperAI(height=HEIGHT, width=WIDTH)
+            revealed = set()
+            flags = set()
+            lost = False
+            continue
+
+        # User-made move
+        elif not lost:
+            for i in range(HEIGHT):
+                for j in range(WIDTH):
+                    if (cells[i][j].collidepoint(mouse)
+                            and (i, j) not in flags
+                            and (i, j) not in revealed):
+                        move = (i, j)
+
+    # Make move and update AI knowledge
+    if move:
+        if game.is_mine(move):
+            lost = True
+        else:
+            nearby = game.nearby_mines(move)
+            revealed.add(move)
+            ai.add_knowledge(move, nearby)
+
+    pygame.display.flip()
